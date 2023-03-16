01de1d316309c7a238c917a0e0525d12e051fa67
Steve Romero <romero.steve.a@gmail.com>
2023-03-15 17:27:39 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(num){
+    return Math.abs(42 - num);
+}
+
+function distanceFromHqInFeet(num){
+    return distanceFromHqInBlocks(num) * 264;
+}
+
+function distanceTravelledInFeet(start, dest){
+    return Math.abs(start - dest) * 264;
+}
+
+function calculatesFarePrice(start, dest){
+    let dist = distanceTravelledInFeet(start, dest);
+
+    if (dist <=400)
+        return 0;
+    
+    if (dist <= 2000)
+        return (dist - 400) * 0.02;
+
+    if (dist <= 2500)
+        return 25;
+
+    return 'cannot travel that far';
+}
